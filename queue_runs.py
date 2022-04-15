770d87dc77f65fdc6621ce8a3918bd46d512161d
pedersor <pedersor@uci.edu>
2022-04-14 22:55:47 -0700

    add queue_runs

---
+++ queue_runs.py
@@ -0,0 +1,49 @@
+import textwrap
+import subprocess
+from pathlib import Path
+
+import pylibxc
+
+xc_funcs = pylibxc.util.xc_available_functional_names()
+
+# out directory
+out_dir = Path('out')
+out_dir.mkdir(parents=True, exist_ok=True)
+
+func_types = ['gga_c_', 'mgga_c_']
+
+for func_type in func_types:
+  for xc_func in xc_funcs:
+    if func_type in xc_func and '_1d_' not in xc_func and '_2d_' not in xc_func:
+
+      run_file = 'run_test.py'
+      job_file = f'{xc_func}.job'
+      with open(out_dir / job_file, "w") as fh:
+        # slurm commands
+        lines = (f"""\
+            #!/bin/bash
+            #SBATCH --job-name="{xc_func}"
+            #SBATCH --account=burke
+            #SBATCH --partition=sib2.9
+            #SBATCH --ntasks=1
+            #SBATCH --nodes=1
+            #SBATCH --cpus-per-task=8
+            #SBATCH --time=10:00:00
+            #SBATCH --mem=16G
+
+            ml purge
+            ml miniconda/3/own
+            srun python {run_file} {xc_func}
+
+        """)
+
+        lines = textwrap.dedent(lines)
+        fh.writelines(lines)
+
+      # slurm batch submit
+      proc = subprocess.Popen(
+          f"sbatch {job_file}",
+          shell=True,
+          cwd=out_dir,
+          stdout=None,
+      )
--- run_test.py
+++ run_test.py
@@ -1,4 +1,5 @@
 import sys
+from pathlib import Path
 
 import pandas as pd
 import numpy as np
@@ -13,53 +14,86 @@ func_c = pylibxc.LibXCFunctional(xc, "polarized")
 df = {
     'xc': [xc],
     'satisfied': [],
+    'r_s_range': [],
+    's_range': [],
+    'zeta_range': [],
 }
 
 range_labels = ['r_s_range', 's_range', 'zeta_range', 'alpha_range', 'q_range']
 
 if 'mgga_c_' in xc:
 
-  r_s = np.linspace(0.001, 2, 50)
-  s = np.linspace(0, 5, 50)
-  zeta = np.linspace(0, 1, 50)
-  alpha = np.linspace(0, 5, 50)
+  df['alpha_range'] = []
+  df['q_range'] = []
 
   if func_c._needs_laplacian:
 
-    # TODO remove
     r_s = np.linspace(0.001, 2, 10)
     s = np.linspace(0, 5, 10)
     zeta = np.linspace(0, 1, 10)
     alpha = np.linspace(0, 5, 10)
-
     q = np.linspace(0, 5.0, 50)
-    input = np.meshgrid(r_s, s, zeta, alpha, q, indexing='ij')
-    eps_c = test_suite.mgga_c_lapl(func_c, *input)
-    cond_satisfied, ranges = test_suite.deriv_check(input, eps_c)
 
-    if ranges is not None:
-      for i, r in enumerate(ranges):
-        df[range_labels[i]] = [r]
+    # split up to reduce memory
+    r_s_splits = np.split(r_s, 5)
+    cond_satisfied = True
+    for r_s_split in r_s_splits:
 
-    else:
+      input = np.meshgrid(r_s_split, s, zeta, alpha, q, indexing='ij')
+      eps_c = test_suite.mgga_c_lapl(func_c, *input)
+      split_cond_satisfied, ranges = test_suite.deriv_check(input, eps_c)
+
+      del input
+      del eps_c
+
+      if not split_cond_satisfied:
+        cond_satisfied = False
+        for i, r in enumerate(ranges):
+          df[range_labels[i]].append(r)
+
+    if cond_satisfied:
       for label in range_labels:
         df[label] = ['---']
+    else:
+      for label in range_labels:
+        min_range = np.amin(df[label])
+        max_range = np.amax(df[label])
+        df[label] = [[min_range, max_range]]
 
   else:
-    input = np.meshgrid(r_s, s, zeta, alpha, indexing='ij')
-    eps_c = test_suite.mgga_c(xc, *input)
-    cond_satisfied, ranges = test_suite.deriv_check(input, eps_c)
+    r_s = np.linspace(0.001, 2, 50)
+    s = np.linspace(0, 5, 50)
+    zeta = np.linspace(0, 1, 50)
+    alpha = np.linspace(0, 5, 50)
 
-    if ranges is not None:
-      for i, r in enumerate(ranges):
-        df[range_labels[i]] = [r]
+    # split up to reduce memory
+    r_s_splits = np.split(r_s, 5)
+    cond_satisfied = True
+    for r_s_split in r_s_splits:
+
+      input = np.meshgrid(r_s_split, s, zeta, alpha, indexing='ij')
+      eps_c = test_suite.mgga_c(xc, *input)
+      split_cond_satisfied, ranges = test_suite.deriv_check(input, eps_c)
+
+      del input
+      del eps_c
+
+      if not split_cond_satisfied:
+        cond_satisfied = False
+        for i, r in enumerate(ranges):
+          df[range_labels[i]].append(r)
+
+    if cond_satisfied:
+      for label in range_labels:
+        df[label] = ['---']
+    else:
+      for label in range_labels[:-1]:
+        min_range = np.amin(df[label])
+        max_range = np.amax(df[label])
+        df[label] = [[min_range, max_range]]
 
       # no lapl.
       df[range_labels[-1]] = ['---']
-
-    else:
-      for label in range_labels:
-        df[label] = ['---']
 
 elif 'gga_c_' in xc:
 
