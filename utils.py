788e8d7d3b1a0c07eb9016479d52e62ab5dc1bf0
pedersor <pedersor@uci.edu>
2022-05-24 23:49:30 -0700

    p86 corr scaling counterexample

--- utils.py
+++ utils.py
@@ -1,10 +1,22 @@
 import numpy as np
 
 
+def get_density(r_s):
+  return 3 / (4 * np.pi * (r_s**3))
+
+
 def get_r_s(n):
   return (4 * np.pi * n / 3)**(-1 / 3)
 
 
-def get_s(n, n_grads):
-  n_grad = np.sum(n_grads**2, axis=0)**(1 / 2)
+def get_s(n, n_grad):
+  if n_grad.ndim == 1:
+    n_grad = np.abs(n_grad)
+  else:
+    n_grad = np.sum(n_grad**2, axis=0)**(1 / 2)
   return n_grad / (2 * (3 * np.pi)**(1 / 3) * n**(4 / 3))
+
+
+def get_grad_n(s, n):
+  """ Obtain |\nabla n| from the reduced gradient s. """
+  return s * (2 * ((3 * np.pi**2)**(1 / 3)) * (n**(4 / 3)))
