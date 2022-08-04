a5eed9858740949868990ea8e061515e97e6f7fd
Ryan Pederson <pedersor@uci.edu>
2022-08-04 11:15:12 -0700

    reorg runs

--- exact_conditions_checker/queue_runs.py
+++ exact_conditions_checker/queue_runs.py
@@ -4,49 +4,70 @@ from pathlib import Path
 
 import pylibxc
 
-func_types = ['gga_xc_', 'mgga_xc_', 'mgga_c_']
-cond_to_check = 'lieb_oxford_bd_check_Exc'
+func_types = [
+    'gga_c_',
+    'gga_xc_',
+    'hyb_gga_xc_',
+    'mgga_c_',
+    'mgga_xc_',
+    'hyb_mgga_xc_',
+]
 
+conditions_to_check = [
+    "negativity_check",
+    "deriv_lower_bd_check",
+    "deriv_upper_bd_check_1",
+    "deriv_upper_bd_check_2",
+    "second_deriv_check",
+    "lieb_oxford_bd_check_Uxc",
+    "lieb_oxford_bd_check_Exc",
+]
+
+range_type = 'unpol_nonzero_s'
+
+# all available XC functionals in libxc
 xc_funcs = pylibxc.util.xc_available_functional_names()
 
-# out directory
-out_dir = Path(cond_to_check)
-out_dir.mkdir(parents=True, exist_ok=True)
+for cond_to_check in conditions_to_check:
 
-for func_type in func_types:
-  for xc_func in xc_funcs:
-    # filter out 1d and 2d functionals
-    if func_type in xc_func and func_type[0] == xc_func[
-        0] and '_1d_' not in xc_func and '_2d_' not in xc_func:
+  # out directory
+  out_dir = Path(cond_to_check)
+  out_dir.mkdir(parents=True, exist_ok=True)
 
-      run_file = '../run_condition_checks.py'
-      job_file = f'{xc_func}.job'
-      with open(out_dir / job_file, "w") as fh:
-        # slurm commands
-        lines = (f"""\
-            #!/bin/bash
-            #SBATCH --job-name="{xc_func}"
-            #SBATCH --account=burke
-            #SBATCH --partition=sib2.9,nes2.8
-            #SBATCH --ntasks=1
-            #SBATCH --nodes=1
-            #SBATCH --cpus-per-task=8
-            #SBATCH --time=10:00:00
-            #SBATCH --mem=32G
+  for func_type in func_types:
+    for xc_func in xc_funcs:
+      # filter out 1d and 2d functionals
+      if func_type in xc_func and func_type[0] == xc_func[
+          0] and '_1d_' not in xc_func and '_2d_' not in xc_func:
 
-            ml purge
-            ml miniconda/3/own
-            srun python {run_file} {xc_func} {cond_to_check}
+        run_file = '../run_condition_checks.py'
+        job_file = f'{xc_func}.job'
+        with open(out_dir / job_file, "w") as fh:
+          # slurm commands
+          lines = (f"""\
+              #!/bin/bash
+              #SBATCH --job-name="{xc_func}"
+              #SBATCH --account=burke
+              #SBATCH --partition=sib2.9,nes2.8
+              #SBATCH --ntasks=1
+              #SBATCH --nodes=1
+              #SBATCH --cpus-per-task=8
+              #SBATCH --time=10:00:00
+              #SBATCH --mem=32G
 
-        """)
+              ml purge
+              ml miniconda/3/own
+              srun python {run_file} {xc_func} {cond_to_check} {range_type}
 
-        lines = textwrap.dedent(lines)
-        fh.writelines(lines)
+          """)
 
-      # slurm batch submit
-      proc = subprocess.Popen(
-          f"sbatch {job_file}",
-          shell=True,
-          cwd=out_dir,
-          stdout=None,
-      )
+          lines = textwrap.dedent(lines)
+          fh.writelines(lines)
+
+        # slurm batch submit
+        proc = subprocess.Popen(
+            f"sbatch {job_file}",
+            shell=True,
+            cwd=out_dir,
+            stdout=None,
+        )
--- exact_conditions_checker/run_condition_checks.py
+++ exact_conditions_checker/run_condition_checks.py
@@ -7,15 +7,15 @@ import condition_checks
 
 func_id = sys.argv[1]
 condition_string = sys.argv[2]
+range_type = sys.argv[3]
 
 print(f"running: {func_id}", flush=True)
 print(f"checking condition: {condition_string}", flush=True)
+print(f"range type: {range_type}", flush=True)
 
 func_c = pylibxc.LibXCFunctional(func_id, "polarized")
 
-comprehensive = False
-
-if comprehensive:
+if range_type == 'comprehensive':
   if 'mgga_c_' in func_id or 'mgga_xc_' in func_id:
     if func_c._needs_laplacian:
       input = {
@@ -48,12 +48,12 @@ if comprehensive:
       input,
       num_splits=num_splits,
   )
-else:
+elif range_type == 'unpol_nonzero_s':
   if 'mgga_c_' in func_id or 'mgga_xc_' in func_id:
     if func_c._needs_laplacian:
       input = {
           'r_s': np.linspace(0.01, 5, 3000),
-          's': np.linspace(0.1, 3, 100),
+          's': np.linspace(0.1, 5, 100),
           'zeta': np.array([0]),
           'alpha': np.linspace(0, 5, 10),
           'q': np.linspace(0, 5, 50),
@@ -62,7 +62,7 @@ else:
     else:
       input = {
           'r_s': np.linspace(0.01, 5, 5000),
-          's': np.linspace(0.1, 3, 100),
+          's': np.linspace(0.1, 5, 100),
           'zeta': np.array([0]),
           'alpha': np.linspace(0, 5, 100),
       }
@@ -70,7 +70,7 @@ else:
   elif 'gga_c_' in func_id or 'gga_xc_' in func_id:
     input = {
         'r_s': np.linspace(0.01, 5, 10000),
-        's': np.linspace(0.1, 3, 500),
+        's': np.linspace(0.1, 5, 500),
         'zeta': np.array([0]),
     }
     num_splits = 100
@@ -81,5 +81,41 @@ else:
       input,
       num_splits=num_splits,
   )
+
+elif range_type == 'unpol_small_s':
+  if 'mgga_c_' in func_id or 'mgga_xc_' in func_id:
+    if func_c._needs_laplacian:
+      input = {
+          'r_s': np.linspace(0.01, 5, 3000),
+          's': np.linspace(0, 0.1, 10),
+          'zeta': np.array([0]),
+          'alpha': np.linspace(0, 5, 10),
+          'q': np.linspace(0, 5, 50),
+      }
+      num_splits = 5
+    else:
+      input = {
+          'r_s': np.linspace(0.01, 5, 5000),
+          's': np.linspace(0, 0.1, 10),
+          'zeta': np.array([0]),
+          'alpha': np.linspace(0, 5, 100),
+      }
+      num_splits = 5
+  elif 'gga_c_' in func_id or 'gga_xc_' in func_id:
+    input = {
+        'r_s': np.linspace(0.01, 5, 10000),
+        's': np.linspace(0, 0.1, 10),
+        'zeta': np.array([0]),
+    }
+    num_splits = 5
+
+  df = condition_checks.check_condition(
+      func_id,
+      condition_string,
+      input,
+      num_splits=num_splits,
+  )
+else:
+  NotImplementedError(f"range_type {range_type} not supported.")
 
 df.to_csv(f'{func_id}.csv', header=False, index=False)
