760f683eb405944c32a1e9d4c98112595900f38d
Ryan Pederson <pedersor@uci.edu>
2022-09-13 14:47:26 -0700

    try additional scf_args if no convergence

--- atomic_systems/evaluator.py
+++ atomic_systems/evaluator.py
@@ -12,10 +12,12 @@ from exact_conds import CondChecker
 
 class PyscfEvaluator():
 
-  def __init__(self, xc, scf_args={}):
+  def __init__(self, xc, scf_args=[{}]):
     xc = xc.lower()
     self.xc = xc
     self.scf_args = scf_args
+
+    # cached mf objects
     self.mfs = None
 
     if xc == "ccsd":
@@ -25,35 +27,53 @@ class PyscfEvaluator():
     else:
       self.calc = "ksdft"
 
+  @property
+  def scf_args(self):
+    return self._scf_args
+
+  @scf_args.setter
+  def scf_args(self, scf_args):
+    if not isinstance(scf_args, list):
+      scf_args = [scf_args]
+    # append additional scf args to try
+    scf_args.extend(self._other_scf_args())
+    self._scf_args = scf_args
+
+  def _other_scf_args(self):
+    """ Additional scf args to try. """
+    other_scf_args = [{'diis': scf.ADIIS()}]
+    return other_scf_args
+
   def run(self, system: System):
 
     mol = system.get_pyscf_system()
     if self.calc == "ksdft":
       # KS-DFT calculation
-
       if mol.spin == 0:
         mf = dft.RKS(mol)
       else:
         mf = dft.UKS(mol)
       mf.xc = self.xc
-      mf = self.use_scf_args(mf)
-      mf.kernel()
     elif self.calc == "hf":
       # HF calculation
-
       if mol.spin == 0:
         mf = scf.RHF(mol)
       else:
         mf = scf.UHF(mol)
-      mf = self.use_scf_args(mf)
+
+    # try default scf_args and others
+    for scf_args in self.scf_args:
+      mf = self._use_scf_args(mf, scf_args)
       mf.kernel()
+      if mf.converged:
+        break
 
     return mf
 
-  def use_scf_args(self, mf):
+  def _use_scf_args(self, mf, scf_args):
 
-    for key in self.scf_args:
-      setattr(mf, key, self.scf_args[key])
+    for key in scf_args:
+      setattr(mf, key, scf_args[key])
 
     return mf
 
