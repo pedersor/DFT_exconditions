5de70807569084092eaf17cb62da95addda82a2d
pedersor <pedersor@uci.edu>
2022-09-13 21:33:24 -0700

    simple ie result heatmap plot

---
+++ atomic_systems/analysis/analysis.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+
+import pandas as pd
+import seaborn as sns
+
+HAR_TO_KCAL = 627.5
+
+out_dir = Path('../out/')
+nonempirical = ['scan', 'pbe', 'r2scan']
+
+errs_df = {'System': []}
+for i, csv in enumerate(out_dir.glob('errs_*.csv')):
+  xc_df = pd.read_csv(csv)
+  if i == 0:
+    # all systems + last entry is the MAE
+    systems = pd.concat([xc_df['label'], pd.Series(['MAE'])])
+    errs_df['System'] = systems
+
+  xc = csv.stem.split('_')[-1].lower()
+  errs = xc_df['error'].abs() * HAR_TO_KCAL
+  errs = pd.concat([errs, pd.Series([errs.mean()])])
+  errs_df[xc] = errs
+
+errs_df = pd.DataFrame.from_dict(errs_df)
+errs_df = errs_df.set_index('System')
+
+
+# sort columns non-empirical -> empirical
+def sort_fn(xc):
+  return int(xc not in nonempirical)
+
+
+cols_to_sort = errs_df.columns
+sorted_cols = sorted(cols_to_sort, key=sort_fn)
+errs_df = errs_df.reindex(sorted_cols, axis=1)
+
+sns.heatmap(
+    errs_df,
+    annot=True,
+    fmt='.2g',
+    cmap="YlGnBu",
+    cbar_kws={'label': 'abs. error [kcal/mol]'},
+)
